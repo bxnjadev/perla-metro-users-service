fc413178a02f1a7fb9af42286a1675ffa4ae45ec
bxnjadev <benjamin.hyx@gmail.com>
2025-09-06 22:06:14 -0400

    feat: added validations

---
+++ src/Dto/CreationUser.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace perla_metro_users_service.Dto;
+
+public class CreationUser
+{
+    
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
+    [StringLength(100, ErrorMessage = "El nombre no puede exceder los 100 caracteres.")]
+    public string Names { get; set; }
+
+    [Required(ErrorMessage = "El apellido es obligatorio.")]
+    [StringLength(100, ErrorMessage = "El apellido no puede exceder los 100 caracteres.")]
+    public string LastNames { get; set; }
+
+    [Required(ErrorMessage = "El correo electrónico es obligatorio.")]
+    [EmailAddress(ErrorMessage = "Formato de correo inválido.")]
+    [RegularExpression(@"^[a-zA-Z0-9._%+-]+@perlametro\.cl$", ErrorMessage = "El correo debe ser institucional (@perlametro.cl).")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "La contraseña es obligatoria.")]
+    [StringLength(100, MinimumLength = 8, ErrorMessage = "La contraseña debe tener al menos 8 caracteres.")]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$", 
+        ErrorMessage = "La contraseña debe contener al menos: una mayúscula, una minúscula, un número y un carácter especial.")]
+    public string Password { get; set; }
+
+    
+}
---
+++ src/service/UserService.cs
@@ -0,0 +1,12 @@
+﻿using perla_metro_users_service.Dto;
+
+namespace perla_metro_users_service.service;
+
+public interface IUserService
+{
+
+    void Create(CreationUser creationUser); 
+    
+    
+    
+}
