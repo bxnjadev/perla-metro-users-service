38bfbc04cf59e65f8939d1df0bdad8704fd66ad1
bxnjadev <benjamin.hyx@gmail.com>
2025-09-06 20:36:36 -0400

    feat: defined base for repository objects

---
+++ src/Repository/IObjectRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace perla_metro_users_service.Repository;
+
+public interface IObjectRepository<O>
+{
+
+    Task<O> StoreAsync(O obj);
+
+    Task<O?> DeleteAsync(string uuid);
+
+    Task<O?> FindByUuid(string uuid);
+
+    Task<O?> Update(string uuid, O obj);
+
+    Task<ICollection<O>> AllAsync();
+
+}
--- src/Repository/UserRepository.cs
+++ src/Repository/UserRepository.cs
@@ -1,8 +1,39 @@
-﻿namespace perla_metro_users_service.Repository;
+﻿using Microsoft.EntityFrameworkCore;
+using perla_metro_users_service.Data;
+using perla_metro_users_service.Model;
 
-public interface UserRepository
+namespace perla_metro_users_service.Repository;
+
+public class UserRepository(
+        ApplicationDbContext dbContext) : IObjectRepository<User>
 {
+
+    private DbSet<User> _users = dbContext.Users;
     
-    
+    public Task<User> StoreAsync(User obj)
+    {
+        
+    }
+
+    public Task<User?> DeleteAsync(string uuid)
+    {
+        
+    }
+
+    public Task<User?> FindByUuid(string uuid)
+    {
+        
+    }
+
+    public Task<User?> Update(string uuid, User obj)
+    {
+        
+    }
+
+    public Task<ICollection<User>> AllAsync()
+    {
+        
+    }
+
     
 }
